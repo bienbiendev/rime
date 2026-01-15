62eb370c6b38489e11c8024c35feee95a9bcf492
bienoubien-studio <salut@bienoubien.studio>
2026-01-15 17:09:54 +0100

    feat: add site mixins to be able to import site for debug purpose

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,6 +4,7 @@ const mixins = require('postcss-mixins');
 const config = {
 	plugins: [
 		mixins({
+			mixinsFiles: ['./src/lib/site/styles/*.mixins.css'],
 			mixinsDir: ['./src/lib/panel/style/mixins', './src/lib/site/styles/mixins']
 		}),
 		autoprefixer
