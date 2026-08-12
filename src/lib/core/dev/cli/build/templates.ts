7448ef3899953bb1a75ebab52156734eb3a6054b
bienoubien-studio <salut@bienoubien.studio>
2026-08-12 18:57:06 +0200

    feat(cli): include process.env.HOST falling back to 127.0.0.1 in server/index.js for production so the process listen only on this specific host and not *:3000

--- src/lib/core/dev/cli/build/templates.ts
+++ src/lib/core/dev/cli/build/templates.ts
@@ -4,10 +4,13 @@ import { handler } from './build/handler.js';
 
 const serve = serveStatic('./static');
 
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || '127.0.0.1';
+
 polka()
 	.use(serve)
 	.use(handler)
-	.listen(3000, () => {
-		console.log("server running");
+	.listen(port, host, () => {
+		console.log(\`server running on \${host}:\${port}\`);
 	});
 `;
