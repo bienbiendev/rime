b03b800d898ad0feeb617242dcd245917dfc5a30
bienoubien-studio <salut@bienoubien.studio>
2026-08-13 20:38:35 +0200

    feat: expose Input ui component

--- src/lib/panel/ui.ts
+++ src/lib/panel/ui.ts
@@ -1,5 +1,6 @@
 // For shared ui components ex: use a panel button in a plugin
 import Button from './components/ui/button/button.svelte';
+import Input from './components/ui/input/Input.svelte';
 import SpinLoader from './components/ui/spin-loader/SpinLoader.svelte';
 
-export { Button, SpinLoader };
+export { Button, Input, SpinLoader };
