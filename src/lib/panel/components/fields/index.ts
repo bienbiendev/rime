954579ae5ed10d2a7ac82f28da1197c2f0c6e584
bienoubien-studio <salut@bienoubien.studio>
2026-08-13 20:34:03 +0200

    feat: export root fieldset directive for custom field definition

--- src/lib/panel/components/fields/index.ts
+++ src/lib/panel/components/fields/index.ts
@@ -2,10 +2,12 @@ import Error from './Error.svelte';
 import Hint from './Hint.svelte';
 import Label from './Label.svelte';
 import LabelFor from './LabelFor.svelte';
+import { root } from './root.svelte.js';
 
 export const Field = {
   Label,
   Hint,
   LabelFor,
-  Error
+  Error,
+  fieldset: root
 };
