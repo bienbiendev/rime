a187b24e87e88ea09577eee1b24b29465458e565
bienbiendev <salut@bienoubien.studio>
2026-09-06 20:13:58 +0200

    Update index.ts

--- src/lib/core/config/index.ts
+++ src/lib/core/config/index.ts
@@ -1,3 +1,4 @@
+// @decouple same as the server version 
 import * as Area from '$lib/core/prototype/area/index.js';
 import * as Collection from '$lib/core/prototype/collection/index.js';
 import { buildConfigClient } from './build.js';
